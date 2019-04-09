1a0e58ee45d05a62f430fe0c862f1fb6546352ce
Cody Bumgardner <cody@uky.edu>
2019-04-09 15:50:48 -0400

    Initial commit

---
+++ src/main/java/edu/uky/cs405g/sample/database/DBEngine.java
@@ -0,0 +1,373 @@
+package edu.uky.cs405g.sample.database;
+
+import org.apache.commons.dbcp2.*;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class DBEngine {
+
+    private DataSource ds;
+
+    public boolean isInit = false;
+
+    public DBEngine(String host, String database, String login, String password) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+
+            String dbConnectionString = null;
+
+
+            if(database == null) {
+
+                dbConnectionString ="jdbc:mysql://" + host + "?" +
+                        "user=" + login  +"&password=" + password + "&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            } else {
+
+                dbConnectionString ="jdbc:mysql://" + host + "/" + database  + "?" +
+                        "user=" + login  +"&password=" + password + "&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            }
+
+            ds = setupDataSource(dbConnectionString);
+
+
+            isInit = true;
+
+
+        }
+
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public static DataSource setupDataSource(String connectURI) {
+        //
+        // First, we'll create a ConnectionFactory that the
+        // pool will use to create Connections.
+        // We'll use the DriverManagerConnectionFactory,
+        // using the connect string passed in the command line
+        // arguments.
+        //
+        ConnectionFactory connectionFactory = null;
+            connectionFactory = new DriverManagerConnectionFactory(connectURI, null);
+
+
+        //
+        // Next we'll create the PoolableConnectionFactory, which wraps
+        // the "real" Connections created by the ConnectionFactory with
+        // the classes that implement the pooling functionality.
+        //
+        PoolableConnectionFactory poolableConnectionFactory =
+                new PoolableConnectionFactory(connectionFactory, null);
+
+        //
+        // Now we'll need a ObjectPool that serves as the
+        // actual pool of connections.
+        //
+        // We'll use a GenericObjectPool instance, although
+        // any ObjectPool implementation will suffice.
+        //
+        ObjectPool<PoolableConnection> connectionPool =
+                new GenericObjectPool<>(poolableConnectionFactory);
+
+        // Set the factory's pool property to the owning pool
+        poolableConnectionFactory.setPool(connectionPool);
+
+        //
+        // Finally, we create the PoolingDriver itself,
+        // passing in the object pool we created.
+        //
+        PoolingDataSource<PoolableConnection> dataSource =
+                new PoolingDataSource<>(connectionPool);
+
+        return dataSource;
+    }
+
+    public Map<String,String> getLocations() {
+        Map<String,String> teamIdMap = new HashMap<>();
+
+        Statement stmt = null;
+        try
+        {
+            Connection conn = ds.getConnection();
+            String queryString = null;
+
+            queryString = "SELECT * FROM location";
+
+            stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(queryString);
+
+            while (rs.next()) {
+                String teamId = rs.getString("lid");
+                String teamName = rs.getString("address");
+
+                teamIdMap.put(teamId, teamName);
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return teamIdMap;
+    }
+
+    public Map<String,String> getLocation(String address) {
+        Map<String,String> locationMap = new HashMap<>();
+
+        Statement stmt = null;
+        try
+        {
+            Connection conn = ds.getConnection();
+            String queryString = null;
+
+            queryString = "SELECT * FROM location WHERE address='" + address + "'";
+
+            stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(queryString);
+
+            while (rs.next()) {
+                String lid = rs.getString("lid");
+                locationMap.put("lid", lid);
+                locationMap.put("address", address);
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return locationMap;
+    }
+
+    public int executeUpdate(String stmtString) {
+        int result = -1;
+        try {
+            Connection conn = ds.getConnection();
+            Statement stmt = conn.createStatement();
+            result = stmt.executeUpdate(stmtString);
+            stmt.close();
+            conn.close();
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return  result;
+    }
+
+    public int dropTable(String tableName) {
+        int result = -1;
+        try {
+            Connection conn = ds.getConnection();
+            String stmtString = null;
+
+            stmtString = "DROP TABLE " + tableName;
+
+            Statement stmt = conn.createStatement();
+
+            result = stmt.executeUpdate(stmtString);
+
+            stmt.close();
+            conn.close();
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+
+    //utility functions
+
+    public boolean databaseExist(String databaseName)  {
+        boolean exist = false;
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = ds.getConnection();
+            String queryString = null;
+
+            queryString = "SELECT COUNT(1) FROM INFORMATION_SCHEMA.SCHEMATA " +
+                    "WHERE SCHEMA_NAME  = N'" + databaseName + "'";
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery(queryString);
+            rs.next();
+            exist = rs.getBoolean(1);
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+            //todo likely better way to do this hack to let derby work
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return exist;
+    }
+
+    public List<String> getDatabaseNames()  {
+
+        List<String> databaseNames = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Connection conn = ds.getConnection();
+            databaseNames = new ArrayList<>();
+
+            String queryString = null;
+
+            queryString = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA";
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery(queryString);
+
+            while (rs.next()) {
+                databaseNames.add(rs.getString(1));
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return databaseNames;
+
+    }
+
+    public List<String> getTableNames(String database)  {
+
+        List<String> tableNames = null;
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = ds.getConnection();
+            tableNames = new ArrayList<>();
+            String queryString = null;
+
+            queryString = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '" + database + "'";
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery(queryString);
+
+            while (rs.next()) {
+                tableNames.add(rs.getString(1));
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return tableNames;
+    }
+
+    public List<String> getColumnNames(String database, String table)  {
+
+        List<String> columnNames = null;
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Connection conn = ds.getConnection();
+            columnNames = new ArrayList<>();
+            String queryString = null;
+
+            queryString = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='" + database + "' AND TABLE_NAME='" + table + "'";
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery(queryString);
+
+            while (rs.next()) {
+                columnNames.add(rs.getString(1));
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return columnNames;
+    }
+
+    public boolean tableExist(String tableName)  {
+        boolean exist = false;
+
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Connection conn = ds.getConnection();
+            String queryString = null;
+
+            queryString = "SELECT COUNT(1) FROM INFORMATION_SCHEMA.TABLES " +
+                    "WHERE TABLE_NAME = N'" + tableName + "'";
+
+            stmt = conn.createStatement();
+
+            rs = stmt.executeQuery(queryString);
+            rs.next();
+            exist = rs.getBoolean(1);
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+
+            //todo likely better way to do this hack to let derby work
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return exist;
+    }
+
+
+
+}
---
+++ src/main/java/edu/uky/cs405g/sample/httpcontrollers/API.java
@@ -0,0 +1,168 @@
+package edu.uky.cs405g.sample.httpcontrollers;
+
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import edu.uky.cs405g.sample.Launcher;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.UUID;
+
+@Path("/api")
+public class API {
+
+    private Type mapType;
+    private Gson gson;
+
+    public API() {
+        mapType = new TypeToken<Map<String, String>>() {
+        }.getType();
+        gson = new Gson();
+    }
+    //curl http://localhost:9998/api/listlocations
+    //{"779a038b-aacc-44ca-b8cc-99671475061f":"800 Rose St.","1e4494a9-5677-49e4-b59f-b77c7900c73f":"123 Campus Road"}
+    @GET
+    @Path("/listlocations")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listTeams(@HeaderParam("X-Auth-API-Key") String authKey) {
+        String responseString = "{}";
+        try {
+            Map<String,String> teamMap = Launcher.dbEngine.getLocations();
+
+            responseString = Launcher.gson.toJson(teamMap);
+
+        } catch (Exception ex) {
+
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            ex.printStackTrace();
+
+            return Response.status(500).entity(exceptionAsString).build();
+        }
+        return Response.ok(responseString).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+    //curl http://localhost:9998/api/getlocation/800%20Rose%20St.
+    //{"address":"800 Rose St.","lid":"c078b038-8ad2-4f45-adf0-03a22fffa8b9"}
+    @GET
+    @Path("/getlocation/{address}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addTeam(@HeaderParam("X-Auth-API-Key") String authKey, @PathParam("address") String address) {
+        String responseString = "{}";
+        try {
+
+            Map<String,String> teamMap = Launcher.dbEngine.getLocation(address);
+
+            responseString = Launcher.gson.toJson(teamMap);
+
+        } catch (Exception ex) {
+
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            ex.printStackTrace();
+
+            return Response.status(500).entity(exceptionAsString).build();
+        }
+        return Response.ok(responseString).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+    //curl http://localhost:9998/api/removelocation/ff2f86ba-ea87-4f5d-8d39-4bdd20b7a532
+    //{"status":"1"}
+    @GET
+    @Path("/removelocation/{location_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteLocation(@HeaderParam("X-Auth-API-Key") String authKey, @PathParam("location_id") String locationId) {
+        String responseString = "{}";
+        try {
+
+
+            String queryString = "delete from location WHERE lid='" + locationId + "'";
+
+            System.out.println(queryString);
+
+            int status = Launcher.dbEngine.executeUpdate(queryString);
+
+            System.out.println("status: " + status);
+
+            responseString = "{\"status\":\"" + status +"\"}";
+
+
+        } catch (Exception ex) {
+
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            ex.printStackTrace();
+
+            return Response.status(500).entity(exceptionAsString).build();
+        }
+        return Response.ok(responseString).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+    //curl -d '{"address":"800 Rose St."}' -H "Content-Type: application/json" -X POST http://localhost:9998/api/addlocation
+    //{"address":"800 Rose St.","lid":"ff2f86ba-ea87-4f5d-8d39-4bdd20b7a532"}
+    @POST
+    @Path("/addlocation")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response crunchifyREST11(InputStream incomingData) {
+
+        StringBuilder crunchifyBuilder = new StringBuilder();
+        String returnString = null;
+        try {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                crunchifyBuilder.append(line);
+            }
+
+            String jsonString = crunchifyBuilder.toString();
+            Map<String, String> myMap = gson.fromJson(jsonString, mapType);
+            String address = myMap.get("address");
+
+            Map<String,String> addressMap = Launcher.dbEngine.getLocation(address);
+
+            if(addressMap.size() == 0) {
+
+                //generate a new unique location Id
+                String locationId = UUID.randomUUID().toString();
+
+                String createUsersTable = "insert into location values ('" + locationId + "','" + address  + "')";
+
+                System.out.println(createUsersTable);
+
+                Launcher.dbEngine.executeUpdate(createUsersTable);
+
+                addressMap = Launcher.dbEngine.getLocation(address);
+
+                returnString = gson.toJson(addressMap);
+
+
+            } else {
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Can't insert duplicate location address!")
+                        .header("Access-Control-Allow-Origin", "*").build();
+            }
+
+
+        } catch (Exception ex) {
+
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            ex.printStackTrace();
+
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Internal Server Error")
+                    .header("Access-Control-Allow-Origin", "*").build();
+        }
+
+        return Response.ok(returnString).header("Access-Control-Allow-Origin", "*").build();
+    }
+
+}
